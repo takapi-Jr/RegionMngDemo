218a194b602185df9f5b456b4706884dc8dabdd1
Takahiro Soda <tsoda@hotmail.co.jp>
2020-07-12 19:56:50 +0900

    PrismのRegionManagerを使って、ViewA、ViewBに遷移

---
+++ RegionMngDemo/RegionMngDemo/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Prism.Mvvm;
+using Prism.Regions;
+using Reactive.Bindings;
+using System;
+
+namespace RegionMngDemo.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Prism Application";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public ReactiveCommand<string> NavigationCommand { get; } = new ReactiveCommand<string>();
+
+        public IRegionManager RegionManager { get; private set; }
+
+        public MainWindowViewModel(IRegionManager regionManager)
+        {
+            this.RegionManager = regionManager;
+
+            NavigationCommand.Subscribe<string>(viewName =>
+            {
+                this.RegionManager.RequestNavigate("ContentRegion", viewName);
+            });
+        }
+    }
+}
